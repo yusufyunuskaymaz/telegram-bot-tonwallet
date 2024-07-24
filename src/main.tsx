be2e2ddda489b89ba1a546df5c50ecee6405276f
Yusuf <kaymazyusuf88@gmail.com>
2024-07-24 16:21:06 +0300

    redirect_added

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,12 @@ import { TonConnectUIProvider } from "@tonconnect/ui-react";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <TonConnectUIProvider manifestUrl="https://telegram-bot-tonwallet.vercel.app/tonconnect-manifest.json">
+    <TonConnectUIProvider
+      actionsConfiguration={{
+        twaReturnUrl: "https://t.me/Tonwallettgamebot/tonwalletgamee",
+      }}
+      manifestUrl="https://telegram-bot-tonwallet.vercel.app/tonconnect-manifest.json"
+    >
       <App />
     </TonConnectUIProvider>
   </React.StrictMode>
